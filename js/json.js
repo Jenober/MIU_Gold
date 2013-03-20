887693972ac58b2c559552cbe4147c500bf776f7
Houston Bennett <Jenober@fullsail.edu>
2013-03-20 13:09:33 -0700

    Gold App Freshness

---
+++ js/json.js
@@ -0,0 +1,372 @@
+<!-- Houston Bennett -->
+<!-- d20 Inspired Character Sheet Archive -->
+<!-- Term 1303 -->
+
+var json = {
+
+    // Various objects with properties, attributes, etc.
+
+    "Default1":{
+
+        "Date": ["Date: ", "12/12/12"],
+        "Name": ['Name: ', "Darmok Un'Jalad Attinagra"],
+        'Race': ['Race: ', 'Dwarf'],
+        "Size": ['Size: ', "Tiny"],
+        "Age": ['Age: ', "132"],
+        "Gender": ["Sex: ",'Male'],
+        "Str": ['Strength: ', '10'],
+        "Con": ['Constitution: ', '10'],
+        "Dex": ['Dexterity: ','10'],
+        "Int": ['Intelligence: ','10'],
+        "Wis": ['Wisdom: ','10'],
+        "Cha": ['Charisma: ', '10'],
+        "Bio": ['Biography: ', '10']
+
+    },
+
+    "Default2":{
+
+        "Date": ["Date: ", "12/12/12"],
+        "Name": ['Name: ', "Trix Arfur Kitz"],
+        "Race": ['Race: ', 'Human'],
+        "Size": ['Size: ', "Tiny"],
+        "Age": ['Age: ', "42"],
+        "Gender": ["Sex: ",'Male'],
+        "Str": ['Strength: ', '11'],
+        "Con": ['Constitution: ', '11'],
+        "Dex": ['Dexterity: ','11'],
+        "Int": ['Intelligence: ','11'],
+        "Wis": ['Wisdom: ','11'],
+        "Cha": ['Charisma: ', '11'],
+        "Bio": ['Biography: ', '11']
+
+    },
+
+    "Default3":{
+
+        "Date": ["Date: ", "12/12/12"],
+        "Name": ['Name: ', "Ed Ippus"],
+        'Race': ['Race: ', 'Gnome'],
+        "Size": ['Size: ', "Tiny"],
+        "Age": ['Age: ', "13"],
+        "Gender": ["Sex: ",'Male'],
+        "Str": ['Strength: ', '10'],
+        "Con": ['Constitution: ', '10'],
+        "Dex": ['Dexterity: ','10'],
+        "Int": ['Intelligence: ','10'],
+        "Wis": ['Wisdom: ','10'],
+        "Cha": ['Charisma: ', '10'],
+        "Bio": ['Biography: ', '10']
+
+    },
+
+    "Default4":{
+
+        "Date": ["Date: ", "12/12/12"],
+        "Name": ['Name: ', "S. H. Rodinger"],
+        "Race": ['Race: ', 'Half-Orc'],
+        "Size": ['Size: ', "Small"],
+        "Age": ['Age: ', "37"],
+        "Gender": ["Sex: ",'Female'],
+        "Str": ['Strength: ', '11'],
+        "Con": ['Constitution: ', '11'],
+        "Dex": ['Dexterity: ','11'],
+        "Int": ['Intelligence: ','11'],
+        "Wis": ['Wisdom: ','11'],
+        "Cha": ['Charisma: ', '11'],
+        "Bio": ['Biography: ', '11']
+
+    },
+
+    "Default5":{
+
+        "Date": ["Date: ", "12/12/12"],
+        "Name": ['Name: ', "Lady Rae"],
+        'Race': ['Race: ', 'Elf'],
+        "Size": ['Size: ', "Small"],
+        "Age": ['Age: ', "412"],
+        "Gender": ["Sex: ",'Female'],
+        "Str": ['Strength: ', '10'],
+        "Con": ['Constitution: ', '10'],
+        "Dex": ['Dexterity: ','10'],
+        "Int": ['Intelligence: ','10'],
+        "Wis": ['Wisdom: ','10'],
+        "Cha": ['Charisma: ', '10'],
+        "Bio": ['Biography: ', '10']
+
+    },
+
+    "Default6":{
+
+        "Date": ["Date: ", "12/12/12"],
+        "Name": ['Name: ', "Alex Morgan"],
+        "Race": ['Race: ', 'Human'],
+        "Size": ['Size: ', "Small"],
+        "Age": ['Age: ', "32"],
+        "Gender": ["Sex: ",'Male'],
+        "Str": ['Strength: ', '11'],
+        "Con": ['Constitution: ', '11'],
+        "Dex": ['Dexterity: ','11'],
+        "Int": ['Intelligence: ','11'],
+        "Wis": ['Wisdom: ','11'],
+        "Cha": ['Charisma: ', '11'],
+        "Bio": ['Biography: ', '11']
+
+    },
+
+    "Default7":{
+
+        "Date": ["Date: ", "12/12/12"],
+        "Name": ['Name: ', "Al Gore"],
+        'Race': ['Race: ', 'Dwarf'],
+        "Size": ['Size: ', "Small"],
+        "Age": ['Age: ', "13"],
+        "Gender": ["Sex: ",'Male'],
+        "Str": ['Strength: ', '10'],
+        "Con": ['Constitution: ', '10'],
+        "Dex": ['Dexterity: ','10'],
+        "Int": ['Intelligence: ','10'],
+        "Wis": ['Wisdom: ','10'],
+        "Cha": ['Charisma: ', '10'],
+        "Bio": ['Biography: ', '10']
+
+    },
+
+    "Default8":{
+
+        "Date": ["Date: ", "12/12/12"],
+        "Name": ['Name: ', "Caladd"],
+        "Race": ['Race: ', 'Human'],
+        "Size": ['Size: ', "Medium"],
+        "Age": ['Age: ', "87"],
+        "Gender": ["Sex: ",'Male'],
+        "Str": ['Strength: ', '11'],
+        "Con": ['Constitution: ', '11'],
+        "Dex": ['Dexterity: ','11'],
+        "Int": ['Intelligence: ','11'],
+        "Wis": ['Wisdom: ','11'],
+        "Cha": ['Charisma: ', '11'],
+        "Bio": ['Biography: ', '11']
+
+    },
+
+    "Default9":{
+
+        "Date": ["Date: ", "12/12/12"],
+        "Name": ['Name: ', "Grok I'Tall"],
+        'Race': ['Race: ', 'Half-Orc'],
+        "Size": ['Size: ', "Medium"],
+        "Age": ['Age: ', "103"],
+        "Gender": ["Sex: ",'Male'],
+        "Str": ['Strength: ', '10'],
+        "Con": ['Constitution: ', '10'],
+        "Dex": ['Dexterity: ','10'],
+        "Int": ['Intelligence: ','10'],
+        "Wis": ['Wisdom: ','10'],
+        "Cha": ['Charisma: ', '10'],
+        "Bio": ['Biography: ', '10']
+
+    },
+
+    "Default10":{
+
+        "Date": ["Date: ", "12/12/12"],
+        "Name": ['Name: ', "Fez Allahbad"],
+        "Race": ['Race: ', 'Human'],
+        "Size": ['Size: ', "Medium"],
+        "Age": ['Age: ', "56"],
+        "Gender": ["Sex: ",'Female'],
+        "Str": ['Strength: ', '11'],
+        "Con": ['Constitution: ', '11'],
+        "Dex": ['Dexterity: ','11'],
+        "Int": ['Intelligence: ','11'],
+        "Wis": ['Wisdom: ','11'],
+        "Cha": ['Charisma: ', '11'],
+        "Bio": ['Biography: ', '11']
+
+    },
+
+    "Default11":{
+
+        "Date": ["Date: ", "12/12/12"],
+        "Name": ['Name: ', "Jamie Frenell"],
+        'Race': ['Race: ', 'Dwarf'],
+        "Size": ['Size: ', "Medium"],
+        "Age": ['Age: ', "171"],
+        "Gender": ["Sex: ",'Female'],
+        "Str": ['Strength: ', '10'],
+        "Con": ['Constitution: ', '10'],
+        "Dex": ['Dexterity: ','10'],
+        "Int": ['Intelligence: ','10'],
+        "Wis": ['Wisdom: ','10'],
+        "Cha": ['Charisma: ', '10'],
+        "Bio": ['Biography: ', '10']
+
+    },
+
+    "Default12":{
+
+        "Date": ["Date: ", "12/12/12"],
+        "Name": ['Name: ', "Stance Izwhyde"],
+        "Race": ['Race: ', 'Dwarf'],
+        "Size": ['Size: ', "Large"],
+        "Age": ['Age: ', "232"],
+        "Gender": ["Sex: ",'Female'],
+        "Str": ['Strength: ', '11'],
+        "Con": ['Constitution: ', '11'],
+        "Dex": ['Dexterity: ','11'],
+        "Int": ['Intelligence: ','11'],
+        "Wis": ['Wisdom: ','11'],
+        "Cha": ['Charisma: ', '11'],
+        "Bio": ['Biography: ', '11']
+
+    },
+
+    "Default13":{
+
+        "Date": ["Date: ", "12/12/12"],
+        "Name": ['Name: ', "Picard Attinagra"],
+        'Race': ['Race: ', 'Human'],
+        "Size": ['Size: ', "Large"],
+        "Age": ['Age: ', "132"],
+        "Gender": ["Sex: ",'Male'],
+        "Str": ['Strength: ', '10'],
+        "Con": ['Constitution: ', '10'],
+        "Dex": ['Dexterity: ','10'],
+        "Int": ['Intelligence: ','10'],
+        "Wis": ['Wisdom: ','10'],
+        "Cha": ['Charisma: ', '10'],
+        "Bio": ['Biography: ', '10']
+
+    },
+
+    "Default14":{
+
+        "Date": ["Date: ", "12/12/12"],
+        "Name": ['Name: ', "Hors Trix "],
+        "Race": ['Race: ', 'Half-Orc'],
+        "Size": ['Size: ', "Large"],
+        "Age": ['Age: ', "32"],
+        "Gender": ["Sex: ",'Female'],
+        "Str": ['Strength: ', '11'],
+        "Con": ['Constitution: ', '11'],
+        "Dex": ['Dexterity: ','11'],
+        "Int": ['Intelligence: ','11'],
+        "Wis": ['Wisdom: ','11'],
+        "Cha": ['Charisma: ', '11'],
+        "Bio": ['Biography: ', '11']
+
+    },
+
+    "Default15":{
+
+        "Date": ["Date: ", "12/12/12"],
+        "Name": ['Name: ', "Cross Mihart"],
+        'Race': ['Race: ', 'Elf'],
+        "Size": ['Size: ', "Large"],
+        "Age": ['Age: ', "794"],
+        "Gender": ["Sex: ",'Female'],
+        "Str": ['Strength: ', '10'],
+        "Con": ['Constitution: ', '10'],
+        "Dex": ['Dexterity: ','10'],
+        "Int": ['Intelligence: ','10'],
+        "Wis": ['Wisdom: ','10'],
+        "Cha": ['Charisma: ', '10'],
+        "Bio": ['Biography: ', '10']
+
+    },
+
+    "Default16":{
+
+        "Date": ["Date: ", "12/12/12"],
+        "Name": ['Name: ', "Gilly Bates"],
+        "Race": ['Race: ', 'Human'],
+        "Size": ['Size: ', "Large"],
+        "Age": ['Age: ', "60"],
+        "Gender": ["Sex: ",'Male'],
+        "Str": ['Strength: ', '11'],
+        "Con": ['Constitution: ', '11'],
+        "Dex": ['Dexterity: ','11'],
+        "Int": ['Intelligence: ','11'],
+        "Wis": ['Wisdom: ','11'],
+        "Cha": ['Charisma: ', '11'],
+        "Bio": ['Biography: ', '11']
+
+    },
+
+    "Default17":{
+
+        "Date": ["Date: ", "12/12/12"],
+        "Name": ['Name: ', "Bob Jeeves"],
+        'Race': ['Race: ', 'Human'],
+        "Size": ['Size: ', "Giant"],
+        "Age": ['Age: ', "132"],
+        "Gender": ["Sex: ",'Male'],
+        "Str": ['Strength: ', '10'],
+        "Con": ['Constitution: ', '10'],
+        "Dex": ['Dexterity: ','10'],
+        "Int": ['Intelligence: ','10'],
+        "Wis": ['Wisdom: ','10'],
+        "Cha": ['Charisma: ', '10'],
+        "Bio": ['Biography: ', '10']
+
+    },
+
+    "Default18":{
+
+        "Date": ["Date: ", "12/12/12"],
+        "Name": ['Name: ', "Stellar Wind"],
+        "Race": ['Race: ', 'Elf'],
+        "Size": ['Size: ', "Giant"],
+        "Age": ['Age: ', "900"],
+        "Gender": ["Sex: ",'Female'],
+        "Str": ['Strength: ', '11'],
+        "Con": ['Constitution: ', '11'],
+        "Dex": ['Dexterity: ','11'],
+        "Int": ['Intelligence: ','11'],
+        "Wis": ['Wisdom: ','11'],
+        "Cha": ['Charisma: ', '11'],
+        "Bio": ['Biography: ', '11']
+
+    },
+
+    "Default19":{
+
+        "Date": ["Date: ", "12/12/12"],
+        "Name": ['Name: ', "Bam Bam Bam"],
+        'Race': ['Race: ', 'Half-Orc'],
+        "Size": ['Size: ', "Giant"],
+        "Age": ['Age: ', "132"],
+        "Gender": ["Sex: ",'Female'],
+        "Str": ['Strength: ', '10'],
+        "Con": ['Constitution: ', '10'],
+        "Dex": ['Dexterity: ','10'],
+        "Int": ['Intelligence: ','10'],
+        "Wis": ['Wisdom: ','10'],
+        "Cha": ['Charisma: ', '10'],
+        "Bio": ['Biography: ', '10']
+
+    },
+
+    "Default20":{
+
+        "Date": ["Date: ", "12/12/12"],
+        "Name": ['Name: ', "Kiki Frash"],
+        "Race": ['Race: ', 'Human'],
+        "Size": ['Size: ', "Giant"],
+        "Age": ['Age: ', "42"],
+        "Gender": ["Sex: ",'Male'],
+        "Str": ['Strength: ', '11'],
+        "Con": ['Constitution: ', '11'],
+        "Dex": ['Dexterity: ','11'],
+        "Int": ['Intelligence: ','11'],
+        "Wis": ['Wisdom: ','11'],
+        "Cha": ['Charisma: ', '11'],
+        "Bio": ['Biography: ', '11']
+
+    }
+
+
+
+
+}
